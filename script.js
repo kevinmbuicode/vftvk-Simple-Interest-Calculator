a8b81b41aa8d1657de523cf719d0ed4d5ee24586
kevinmbuicode <kevinmushan@gmail.com>
2021-05-15 17:19:46 +0300

    modified amount input validation

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ function compute()
     var interest = principal * years * rate/100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML = "If you deposit " + principal + "<br/> at an interest rate of " + rate + "% <br/>You will receive an amount of "+ interest + "<br/> in the year " + year;
+    if (principal.value == "") {
+    	alert("Please enter principal amount")
+    } else {
+    	document.getElementById("result").innerHTML = "If you deposit " + principal + "<br/> at an interest rate of " + rate + "% <br/>You will receive an amount of "+ interest + "<br/> in the year " + year;
+    }
 }
 function updateRate() {
     	//reads the rate value slided by the user and updates the rate reading.
     	var rateval = document.getElementById("rate").value;
     	document.getElementById("rate_val").innerHTML = rateval;
     }
+
+
+//document.getElementById("result").innerHTML = "If you deposit " + principal + "<br/> at an interest rate of " + rate + "% <br/>You will receive an amount of "+ interest + "<br/> in the year " + year;
